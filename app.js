c3a7e24c13f0147bd9f6e70977605dc549875732
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-08 10:37:15 +0200

    505. writing typescript express.js code

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port port!`))
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const port = 3000;
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.listen({ port: 3000 });
