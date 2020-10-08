40675db7bc8829e13d677a833bccbca2a0418823
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-08 10:03:06 +0200

    clean up & set up basic rest api with ts

--- app.js
+++ app.js
@@ -1,39 +1,6 @@
-"use strict";
-const num1Element = document.getElementById('num1');
-const num2Element = document.getElementById('num2');
-const buttonElement = document.querySelector('button');
-const numResults = [];
-const textResults = [];
-//union types
-function add(num1, num2) {
-    //type guard
-    if (typeof num1 === 'number' && typeof num2 === 'number') {
-        return num1 + num2;
-    }
-    else if (typeof num1 === 'string' && typeof num2 === 'string') {
-        return num1 + '' + num2;
-    }
-    return +num1 + +num2;
-}
-function printResult(resultObj) {
-    console.log(resultObj.val);
-}
-buttonElement.addEventListener('click', () => {
-    const num1 = num1Element.value;
-    const num2 = num2Element.value;
-    //only string or numbers
-    const result = add(+num1, +num2);
-    const stringResult = add(num1, num2);
-    numResults.push(result);
-    textResults.push(stringResult);
-    printResult({ val: result, timestamp: new Date() });
-    console.log(numResults, textResults);
-});
-const myPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('It worked!');
-    }, 1000);
-});
-myPromise.then((result) => {
-    console.log(result.split('w'));
-});
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port port!`))
