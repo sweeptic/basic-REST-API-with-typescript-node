96efa7381ff99685eeb9eb02957ccbece70f44fe
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-08 09:41:40 +0200

    generics

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ const num1Element = document.getElementById('num1') as HTMLInputElement;
 const num2Element = document.getElementById('num2') as HTMLInputElement;
 const buttonElement = document.querySelector('button')!;
 
-const numResults: number[] = []
+const numResults: Array<number> = []
 const textResults: string[] = []
 
 type NumOrString = number | string;
@@ -46,4 +46,17 @@ buttonElement.addEventListener('click', () => {
    printResult({ val: result as number, timestamp: new Date() });
 
    console.log(numResults, textResults)
+})
+
+
+const myPromise = new Promise<string>(
+   (resolve, reject) => {
+      setTimeout(() => {
+         resolve('It worked!')
+      }, 1000)
+   }
+)
+
+myPromise.then((result) => {
+   console.log(result.split('w'))
 })
